7fe2e1a70ee1dd78912981126a9ebd2cd583634f
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2018-08-09 20:25:53 +0100

    feat(catalogue-grid) implements grid on landing page

--- imports/plugins/custom/custom-home-page/client/components/Grid/index.js
+++ imports/plugins/custom/custom-home-page/client/components/Grid/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Components, getComponent } from "/imports/plugins/core/components/lib";
+import TwoSidedGrid from './components/'
 import "./index.css";
+
+console.log(Components.Products)
+
+const Products = getComponent("Products")
 
 const Grid = () => {
   return (
-    <div>
-      This is the Grid. This is for dika and hasstrup
+    <div style={{ minHeight: '600px', width: "100%"}}>
+      <TwoSidedGrid />
+      <Products />
     </div>
   );
 };
