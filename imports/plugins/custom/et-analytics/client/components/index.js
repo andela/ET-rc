c10c5d7975aa7b4b1427d6a554eb43aaa02bcf13
Solomon Okwa <dikaeinstein@github.com>
2018-08-29 12:36:59 +0100

    feat(analytics): - setup analytics route - write methods for computing analytics data - design and write components

--- imports/plugins/custom/et-analytics/client/components/index.js
+++ imports/plugins/custom/et-analytics/client/components/index.js
@@ -1,27 +1,71 @@
 import React, { Component } from "react";
 import { Meteor } from "meteor/meteor";
 import ProductList from "./ProductList";
+import RenderAnalyticsDetails from "./RenderAnalyticsDetails";
 
 /**
  * Renders the Analytics component
  * @class
  */
-class Analytics extends Component {
+export default class Analytics extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      productTotal: [],
+      status: [],
+      grand: 0,
+      completed: 0,
+      processing: 0,
+      canceled: 0,
+      totalOrdered: 0,
       products: []
     };
     this.fetchTopTenProducts = this.fetchTopTenProducts.bind(this);
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
+    Meteor.call("grand.total", (error, result) => {
+      if (error) {
+        return error;
+      }
+      this.setState({ grand: result[0].grandTotal });
+    });
+    Meteor.call("orders.status", (error, result) => {
+      if (error) {
+        return error;
+      }
+      let completed = 0;
+      let processing = 0;
+      let canceled = 0;
+      for (let i = 0; i < result.length; i += 1) {
+        if ((result[i]._id)[0] === "coreOrderItemWorkflow/completed") {
+          completed = result[i].status;
+        } else if ((result[i]._id)[0] === "coreOrderItemWorkflow/canceled") {
+          canceled = result[i].status;
+        } else if ((result[i]._id)[0] === "coreOrderItemWorkflow/processing") {
+          processing = result[i].status;
+        }
+      }
+      this.setState({ completed, canceled, processing });
+    });
+    Meteor.call("product.total.price", (error, result) => {
+      if (error) {
+        return error;
+      }
+      this.setState({ productTotal: result });
+    });
+    Meteor.call("orders.total", (error, result) => {
+      if (error) {
+        return error;
+      }
+      this.setState({ totalOrdered: result[0].total });
+    });
     this.fetchTopTenProducts();
   }
 
   /**
-   * Fetch top ten products
-   */
+  * Fetch top ten products
+  */
   fetchTopTenProducts() {
     Meteor.call("analytics/topTenProducts", (error, response) => {
       if (error) {
@@ -32,24 +76,33 @@ class Analytics extends Component {
   }
 
   render() {
-    const { products } = this.state;
-    if (products.length < 1) {
-      return (
-        <div className="text-center">
-          <h4><strong>No product has been ordered</strong></h4>
-        </div>
-      );
-    }
+    const {
+      grand, completed, canceled,
+      processing, totalOrdered, products
+    } = this.state;
 
     return (
       <div>
-        <h4 className="text-center mt">
-          <strong>Top Ten Selling Products</strong>
-        </h4>
-        <ProductList products={products} />
+        <RenderAnalyticsDetails
+          grand={grand}
+          completed={completed}
+          processing={processing}
+          canceled={canceled}
+          totalOrdered={totalOrdered}
+        />
+        {products.length < 1
+          ?
+          <div className="text-center">
+            <h4><strong>No product has been ordered</strong></h4>
+          </div>
+          :
+          <div>
+            <h4 className="text-center mt">
+              <strong>Top Ten Selling Products</strong>
+            </h4>
+            <ProductList products={products} />
+          </div>}
       </div>
     );
   }
 }
-
-export default Analytics;
--- imports/plugins/custom/et-analytics/server/methods/analytics.js
+++ imports/plugins/custom/et-analytics/server/methods/analytics.js
@@ -1,11 +1,58 @@
 import { Meteor } from "meteor/meteor";
 import { Orders } from "../../../../../../lib/collections";
 
+
+/**
+ * Methods
+ * @returns {Object} Analytics
+ */
 Meteor.methods({
+  "orders.status"() {
+    return Orders.aggregate([
+      { $group: {
+        _id: "$items.workflow.status",
+        status: { $sum: 1 }
+      } }
+    ]);
+  },
+  "grand.total"() {
+    return Orders.aggregate([
+      { $match: { "workflow.status": "coreOrderWorkflow/completed" } },
+      { $unwind: "$items" },
+      { $unwind: { path: "$billing" } },
+      { $group: {
+        _id: null,
+        grandTotal: { $sum: "$billing.paymentMethod.amount" }
+      }
+      }
+    ]);
+  },
+  "orders.total"() {
+    return Orders.aggregate([
+      { $match: { "workflow.status": "coreOrderWorkflow/completed" } },
+      { $group: {
+        _id: null,
+        total: { $sum: 1 }
+      }
+      }
+    ]);
+  },
+  "product.total.price"() {
+    return Orders.aggregate([
+      { $unwind: "$items" },
+      { $match: { "workflow.status": "coreOrderWorkflow/completed" } },
+      { $group: {
+        _id: "$items.productId",
+        totalOrdered: { $sum: "$items.quantity" },
+        total: { $sum: "$items.variants.price" }, // multiply by quantity sold
+        totalOrdersMade: { $sum: 1 }
+      } }
+    ]);
+  },
   "analytics/topTenProducts": function () {
     return Orders.aggregate([
       { $unwind: { path: "$items" } },
-      { $match: { "items.quantity": { $gt: 0 }, "workflow.status": "coreOrderWorkflow/canceled" } },
+      { $match: { "items.quantity": { $gt: 0 }, "workflow.status": "coreOrderWorkflow/completed" } },
       { $unwind: { path: "$billing" } },
       { $group: {
         _id: "items.productId",
