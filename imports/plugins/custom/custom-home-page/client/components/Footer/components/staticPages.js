1a80ce03265da369432a2ae1c503f3157e9c9653
nkemjiks <mbonunkemjika@ymail.com>
2018-08-30 15:17:31 +0100

    feat: Embed Social Media       - Include twitter and facebook feeds in the footer of landing page     [Finishes #159361721]

--- imports/plugins/custom/custom-home-page/client/components/Footer/components/staticPages.js
+++ imports/plugins/custom/custom-home-page/client/components/Footer/components/staticPages.js
@@ -18,8 +18,22 @@ class Footer extends Component {
 
   componentDidMount = () => {
     this.setState({ pages: this.props.pages });
+    const scriptNode = document.getElementById("twitter-wjs");
+    if (scriptNode) {
+      scriptNode.parentNode.removeChild(scriptNode);
+    }
+    !(function (data, newScript, id) {
+      let file;
+      const element = data.getElementsByTagName(newScript)[0];
+      const TweetsUrl = /^http:/.test(data.location) ? "http" : "https";
+      if (!data.getElementById(id)) {
+        file = data.createElement(newScript);
+        file.id = id;
+        file.src = TweetsUrl + "://platform.twitter.com/widgets.js";
+        element.parentNode.insertBefore(file, element);
+      }
+    }(document, "script", "twitter-wjs"));
   }
-
   componentDidUpdate = (prevProps) => {
     if (this.props.pages.length === prevProps.pages.length) return;
     return this.setState({ pages: this.props.pages });
@@ -33,7 +47,7 @@ class Footer extends Component {
     const { pages } = this.state;
     const fakePages = (
       <div className="static-pages-feed">
-        <h4>EXPLORE</h4>
+        <h4>Explore</h4>
         <p>About Us</p>
         <p>Contact Us</p>
         <p>Terms and Conditions</p>
@@ -42,12 +56,12 @@ class Footer extends Component {
     );
     const realPages = (
       <div className="static-pages-feed">
-        <h4>EXPLORE</h4>
-        { pages && pages.length && pages.map(page => {
+        <h4>Explore</h4>
+        {pages && pages.length && pages.map(page => {
           return (
             <p id={page._id}>
               <a onClick={() => this.visitThisPage(page)}>
-                { page.pageTitle }
+                {page.pageTitle}
               </a>
             </p>
           );
@@ -64,10 +78,39 @@ class Footer extends Component {
       <div className="footer-content">
         <div className="footer-container">
           {this.renderStaticPagesComponent()}
+
           <div className="twitter-container">
-            <h4>TWEET FEEDS</h4>
-            <div>Tweet</div>
+            <h4>Twitter Feeds</h4>
+            <div>
+              <a className="twitter-timeline"
+                data-width="300" data-height="400"
+                href="https://twitter.com/ETWorld4?ref_src=twsrc%5Etfw"
+              >Tweets by ETWorld4</a>
+            </div>
           </div>
+
+          <div className="facebook-container">
+            <h4>Facebook</h4>
+            <iframe
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fetworldrc&tabs=timeline&width=400&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1014279048630887"
+              width="300" height="400" style={{ border: "none", overflow: "hidden" }}
+              scrolling="no" frameBorder="0" allowTransparency="true" allow="encrypted-media"
+            />
+          </div>
+
+          <div className="social-media">
+            <h4>We are here for you</h4>
+            <a href="mailto:et.rc.world@gmail.com">
+              <i className="fa fa-envelope" />
+            </a>
+            <a href="https://www.facebook.com/etworldrc/">
+              <i className="fa fa-facebook-f" />
+            </a>
+            <a href="https://twitter.com/ETWorld4">
+              <i className="fa fa-twitter" />
+            </a>
+          </div>
+
         </div>
         <div className="footer-end">
           <p>&#9728; Made in Big Apple, Epic Towers by ET-Team &copy; 2018</p>
